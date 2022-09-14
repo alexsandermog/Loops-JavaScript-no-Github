ae1d022362e256d540396c97c1b89d7e84485ca4
Alexsander Machado <79030781+alexsandermog@users.noreply.github.com>
2022-09-13 21:48:00 -0300

    Add files via upload

---
+++ Loops-JavaScript-no-Github/atividade2.js
@@ -0,0 +1,9 @@
+//Ler 10 números e imprimir quantos são pares e quantos são ímpares. (FOR)
+
+
+for(let i= 0; i<= 10; i++) {
+if( i % 2 ===0 ) 
+document.write(i,'par')
+
+else document.write(i,'impar')
+}
